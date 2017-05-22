c59b739e84fd1e16bc643dcce9aac469cfaf4234
ben <tben@vip.qq.com>
2017-05-23 01:32:53 +0800

    commit

--- BDemo/FirstViewController.m
+++ BDemo/FirstViewController.m
@@ -25,7 +25,7 @@ typedef void(^StringBlock)();
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"%@", [NSObject new]);
+//    NSLog(@"%@", [NSObject new]);
 }
 
 - (void)didReceiveMemoryWarning {
